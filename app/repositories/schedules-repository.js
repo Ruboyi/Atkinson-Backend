4c77177fdd46b047662b2d78a172f051372cf19b
Gradhoc - rlopez <108720344+rlopezcofrico@users.noreply.github.com>
2023-10-06 20:54:16 +0200

    Update schedules-repository.js

--- app/repositories/schedules-repository.js
+++ app/repositories/schedules-repository.js
@@ -2,16 +2,18 @@
 
 const getPool = require('../infrastructure/database-infrastructure')
 
+//Si es isDelete es 1 no mostrarlo
+
 async function getAllSchedules() {
     const pool = await getPool()
-    const sql = `SELECT * FROM schedules`
+    const sql = `SELECT * FROM schedules WHERE isDelete = 0`
     const [schedules] = await pool.query(sql)
     return schedules
 }
 
 async function getAllSchedulesNew() {
     const pool = await getPool()
-    const sql = `SELECT * FROM schedule_barbers`
+    const sql = `SELECT * FROM schedule_barbers WHERE isDelete = 0`
     const [schedules] = await pool.query(sql)
     return schedules
 }
