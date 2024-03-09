ae7f3aee3401e75251ae7f2e4add9acbd33b8e43
Gradhoc - rlopez <108720344+rlopezcofrico@users.noreply.github.com>
2024-03-09 13:53:07 +0100

    UPDATE BUG BARBER

--- app/repositories/barbershops-repository.js
+++ app/repositories/barbershops-repository.js
@@ -4,14 +4,14 @@ const getPool = require('../infrastructure/database-infrastructure')
 
 async function getAllBarberShops() {
     const pool = await getPool()
-    const sql = `SELECT * FROM barberShops`
+    const sql = `SELECT * FROM barbershops`
     const [barberShops] = await pool.query(sql)
     return barberShops
 }
 
 async function getBarberShopById(idBarberShop) {
     const pool = await getPool()
-    const sql = `SELECT * FROM barberShops WHERE idBarberShop = ?`
+    const sql = `SELECT * FROM barberShops WHERE idBarbershop = ?`
     const [barberShop] = await pool.query(sql, idBarberShop)
     return barberShop[0]
 }
